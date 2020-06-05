ce86eabac316467d6fe9fbbe7ce984d8d180dae5
Anisio Mandlate <anisiomandlate@gmail.com>
2020-06-05 17:46:50 +0200

    feat: Add the routes to server to render styles and organized the files

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.static("/app"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/registar-ponto", (req, res) => {
+  res.sendFile(__dirname + "/src/views/registar-ponto.html");
+});
+
+app.get("/search-results", (req, res) => {
+  res.sendFile(__dirname + "/src/views/search-results.html");
 });
 
 const port = 8000;
